d0d1bdea63641d8f9ab7de799abf9fbcc1fec426
zmplly <1776321381@qq.com>
2021-12-13 23:53:39 +0800

    feat: 泛型类

--- 92.泛型/泛型接口.ts
+++ 92.泛型/泛型接口.ts
@@ -2,3 +2,15 @@
 interface GenericIdentityFn<T> {
   (arg: T): T;
 }
+
+// 泛型类
+
+class GenericNumber<T> {
+  zeroValue: T;
+  add: (x: T, y: T) => T;
+ }
+ let myGenericNumber = new GenericNumber<number>();
+ myGenericNumber.zeroValue = 0;
+ myGenericNumber.add = function (x, y) {
+  return x + y;
+ };
