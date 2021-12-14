1fa1d46c8b9d236ac52eede0c85b8bafe940811d
lly <1776321381@qq.com>
2021-12-14 16:49:23 +0800

    feat: 泛型接口和泛型类

--- 92.泛型/泛型接口和泛型类.ts
+++ 92.泛型/泛型接口和泛型类.ts
@@ -4,13 +4,13 @@ interface GenericIdentityFn<T> {
 }
 
 // 泛型类
-
 class GenericNumber<T> {
   zeroValue: T;
   add: (x: T, y: T) => T;
- }
- let myGenericNumber = new GenericNumber<number>();
- myGenericNumber.zeroValue = 0;
- myGenericNumber.add = function (x, y) {
+}
+let myGenericNumber = new GenericNumber<number>();
+
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function(x, y) {
   return x + y;
- };
+}
